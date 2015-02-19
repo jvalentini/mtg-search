fa9e05a93451b1e378dcf4405bccdafe2258fabc
Justin Valentini <justin.valentini@gmail.com>
2015-02-18 22:37:52 -0500

    Add elastisch, connect to cluster, and index the test.json cards.
    Additionally run a query to find all matches.

--- src/mtg_search/core.clj
+++ src/mtg_search/core.clj
@@ -1,4 +1,23 @@
 (ns mtg-search.core
-  (:require [clojure.data.json :as json]))
+  (:require [clojurewerkz.elastisch.rest :as esr]
+            [clojurewerkz.elastisch.rest.index :as esi]
+            [clojurewerkz.elastisch.rest.document :as esd]
+            [clojurewerkz.elastisch.query         :as q]
+            [clojurewerkz.elastisch.rest.response :as esrsp]
+            [clojure.data.json :as json]
+            [clojure.pprint :refer [pprint]]))
 
 (def sets (json/read-str (slurp "resources/test.json")))
+
+(def conn (esr/connect "https://p60kwkn5be:gm92hykkfm@mtg-3464019877.us-east-1.bonsai.io"))
+
+(defn index [card]
+  (esd/create conn "mtg" "card" card))
+
+(doall (map index sets))
+
+(let [res  (esd/search conn "mtg" "card" :query (q/wildcard :imageName "*"))
+      n    (esrsp/total-hits res)
+      hits (esrsp/hits-from res)]
+  (println (format "Total hits: %d" n))
+  (pprint hits))
